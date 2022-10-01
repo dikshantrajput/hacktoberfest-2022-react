2230a082cca44f0083a2ba5a36bb2f79256dbe0f
Mahesh Jain <48172780+maheshjainckd@users.noreply.github.com>
2022-10-01 18:47:11 +0530

    added hyperlinks

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,11 @@ import './index.css'
 
 function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+     <a href="https://hacktoberfest.com/" target="_blank">HacktoberFest Official Website</a>
+     <a href="https://github.com/dikshantrajput/hacktoberfest-2022-react" target="_blank">Contribute on Github</a>
+     <a href="https://twitter.com/Dikshantrajpu20" target="_blank">Follow me on Twitter</a>
+    </footer>
   )
 }
 
