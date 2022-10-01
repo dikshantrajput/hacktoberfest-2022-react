17402aedbc15fb799304f8ac19fe1ff23ec00102
Mahesh Jain <48172780+maheshjainckd@users.noreply.github.com>
2022-10-01 18:51:32 +0530

    added and formatted hyperlinks in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,9 @@ import './index.css'
 function Footer() {
   return (
     <footer>
-     <a href="https://hacktoberfest.com/" target="_blank">HacktoberFest Official Website</a>
-     <a href="https://github.com/dikshantrajput/hacktoberfest-2022-react" target="_blank">Contribute on Github</a>
-     <a href="https://twitter.com/Dikshantrajpu20" target="_blank">Follow me on Twitter</a>
+     <a href="https://hacktoberfest.com/" target="_blank">HacktoberFest Official Website </a>
+     <a href="https://github.com/dikshantrajput/hacktoberfest-2022-react" target="_blank"> Contribute on Github </a>
+     <a href="https://twitter.com/Dikshantrajpu20" target="_blank"> Follow me on Twitter </a>
     </footer>
   )
 }
