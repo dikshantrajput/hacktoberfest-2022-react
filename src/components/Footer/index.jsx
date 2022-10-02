1d6b28c616b0f1b47b1d178408cb1aaf9ea56b66
Iresh Madushanka <ireshmadushanka03@gmail.com>
2022-10-02 20:19:26 +0530

    fixed button padding issues and styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import './index.css'
-import { useSelector } from "react-redux";
+import React from 'react';
+import './index.css';
+import { useSelector } from 'react-redux';
 
 function Footer() {
   const mode = useSelector((state) => state.mode.value);
 
   return (
-    <footer className={mode ? "footer__dark text-center" : "footer__light text-center"}>
-      <a href="https://hacktoberfest.com/" target="_blank" rel="noreferrer">
-        HacktoberFest Official Website{" "}
+    <footer
+      className={
+        mode ? 'footer__dark text-center' : 'footer__light text-center'
+      }
+    >
+      <a
+        className="footer-a"
+        href="https://hacktoberfest.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        HacktoberFest Official Website{' '}
       </a>
       <a
+        className="footer-a"
         href="https://github.com/dikshantrajput/hacktoberfest-2022-react"
         target="_blank"
         rel="noreferrer"
       >
-        {" "}
-        Contribute on Github{" "}
+        {' '}
+        Contribute on Github{' '}
       </a>
-      <a href="https://twitter.com/Dikshantrajpu20" target="_blank" rel="noreferrer">
-        {" "}
-        Follow me on Twitter{" "}
+      <a
+        className="footer-a"
+        href="https://twitter.com/Dikshantrajpu20"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {' '}
+        Follow me on Twitter{' '}
       </a>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,34 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import Repos from '../../repos.json'
-import './index.css'
-import RepoCard from '../../styled-components/RepoCard'
-import PrinmaryButton from '../../styled-components/Buttons/primary'
+import React, { useEffect, useState } from 'react';
+import Repos from '../../repos.json';
+import './index.css';
+import RepoCard from '../../styled-components/RepoCard';
+import PrinmaryButton from '../../styled-components/Buttons/primary';
 
 function Home() {
-  const [repos,setRepos] = useState([])
-  
+  const [repos, setRepos] = useState([]);
+
   useEffect(() => {
-    if(Repos.repos){
-      setRepos(Repos.repos)  
+    if (Repos.repos) {
+      setRepos(Repos.repos);
     }
-  }, [])
-  
+  }, []);
+
   return (
     <div>
-      Contribute now on any one of these and get your profile listed in the contributors section
-      <span style={{float:'right'}}>
-        <PrinmaryButton link={'https://reactjs.org/'} text={"Learn more about React here"} external={false}/>
-      </span>
-      
+      Contribute now on any one of these and get your profile listed in the
+      contributors section
+      <div style={{ float: 'right' }}>
+        <PrinmaryButton
+          link={'https://reactjs.org/'}
+          text={'Learn more about React here'}
+          external={false}
+          style={{ width: '12rem !important' }}
+        />
+      </div>
       <div className="cards text-center">
-        {
-          repos.map((repo)=>{
-            return <RepoCard key={repo.id} title={repo.title} body={repo.body} btnLink={repo.redirectTo} btnText={"Contribute"} />
-          })
-        }
+        {repos.map((repo) => {
+          return (
+            <RepoCard
+              key={repo.id}
+              title={repo.title}
+              body={repo.body}
+              btnLink={repo.redirectTo}
+              btnText={'Contribute'}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
