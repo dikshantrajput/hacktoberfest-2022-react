f789ac48fa592b1c8b45ec53db5ccb8738860e34
Kayla Kremer <kkremer@getharvest.com>
2022-10-03 15:47:08 -0400

    refactor: have PrimaryButton use valid HTML (no anchor tag inside of button tag) & improve PrimaryButton styles

--- src/styled-components/Buttons/primary/index.jsx
+++ src/styled-components/Buttons/primary/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './index.css'
+import React from "react"
+import "./index.css"
 
-function PrimaryButton({text, link, external = false}) {
-  return (
-    <button className='primary__button'>
-      <a href={link} target={`${external ? '__dk' : ''}`}>
-        {text}
-      </a>
-    </button>
-  )
+function PrimaryButton({ text, link, external = false }) {
+    return (
+        <div className="primary__button">
+            <a href={link} target={`${external ? "__dk" : ""}`}>
+                {text}
+            </a>
+        </div>
+    )
 }
 
 export default PrimaryButton
