bdc63ee46ecb19b1dc049291eafafe5d8024bab3
Devansh Choudhary <choudhary.devansh1007@gmail.com>
2022-10-04 10:02:20 +0530

    countdown added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import {
-  Routes,
-  Route
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Contributors from "./components/Contributors";
-import './app.css';
-import { useSelector } from 'react-redux'
+import "./app.css";
+import { useSelector } from "react-redux";
+import Countdown from "./components/Countdown";
 function App() {
   const mode = useSelector((state) => state.mode.value);
   console.log(mode);
   return (
-    <div className={mode ? "app__container" : "app_container_light"}>
-      <div className="body__container">
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contributors" element={<Contributors />} />
-          </Routes>
+    <>
+      <div className={mode ? "app__container" : "app_container_light"}>
+        <div className="body__container">
+          <Header />
+          <Countdown />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contributors" element={<Contributors />} />
+            </Routes>
+          </div>
+        </div>
+        <div className="footer__container">
+          <Footer />
         </div>
       </div>
-      <div className="footer__container">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Countdown.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Countdown = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <iframe
+        src="https://free.timeanddate.com/countdown/i8j4kz6o/n176/cf111/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cac000/cpc000/pc66c/tc66c/fn3/fs100/szw320/szh135/tatHacktoberfest%20%20ends%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/iso2022-10-31T23:59:59/bo3"
+        allowtransparency="true"
+        frameborder="0"
+        width="322"
+        height="137"
+      ></iframe>
+      ;
+    </div>
+  );
+};
+
+export default Countdown;
