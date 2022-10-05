35d185554d5ec2b7d06749749b2b6011cd9f4ab6
Akshat jain <62347282+akshatjai258@users.noreply.github.com>
2022-10-06 00:35:58 +0530

    Added url for particular repository in contributors page

--- src/components/RepoContributors/index.jsx
+++ src/components/RepoContributors/index.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { getData } from '../../helpers/Api'
-import SecondaryButton from '../../styled-components/Buttons/secondary'
-import ContributorCard from '../../styled-components/ContributorCard'
-import './index.css'
+import React, { useEffect, useState } from "react";
+import { getData } from "../../helpers/Api";
+import SecondaryButton from "../../styled-components/Buttons/secondary";
+import ContributorCard from "../../styled-components/ContributorCard";
+import "./index.css";
 
-function RepoContributors({repo}) {
-  const [pageNo, setPageNo] = useState(1)
-  const [contributors, setContributors] = useState([])
-  const [loadMore, setLoadMore] = useState(true)
+function RepoContributors({ repo }) {
+  const [pageNo, setPageNo] = useState(1);
+  const [contributors, setContributors] = useState([]);
+  const [loadMore, setLoadMore] = useState(true);
 
-  const getAllContributorsOfRepo = async (userName,repoName,page)=>{
-    let options = {}
-    let url = `https://api.github.com/repos/${userName}/${repoName}/contributors?per_page=10&page=${page}`
-    const repoContributos = await getData(url,options)
-    setContributors(repoContributos)
-    if(repoContributos.length <10) {
-    return setLoadMore(false)
+  const getAllContributorsOfRepo = async (userName, repoName, page) => {
+    let options = {};
+    let url = `https://api.github.com/repos/${userName}/${repoName}/contributors?per_page=10&page=${page}`;
+    const repoContributos = await getData(url, options);
+    setContributors(repoContributos);
+    if (repoContributos.length < 10) {
+      return setLoadMore(false);
     }
-  }
+  };
 
-  const loadMoreContributors = ()=>{
-    setPageNo((prev)=>prev+1)
-    getAllContributorsOfRepo(repo?.username, repo?.reponame,pageNo)
-  }
+  const loadMoreContributors = () => {
+    setPageNo((prev) => prev + 1);
+    getAllContributorsOfRepo(repo?.username, repo?.reponame, pageNo);
+  };
 
   useEffect(() => {
-    getAllContributorsOfRepo(repo?.username, repo?.reponame, pageNo)
-  }, [pageNo, repo?.reponame, repo?.username])
-  
+    getAllContributorsOfRepo(repo?.username, repo?.reponame, pageNo);
+  }, [pageNo, repo?.reponame, repo?.username]);
+
   return (
     <div>
-      <h2 className="repo__name">{repo?.reponame}</h2>
+      <a href={repo.redirectTo}>
+        <h2 className="repo__name">{repo?.reponame}</h2>
+      </a>
       <div className="contributor__container">
         {contributors.map((contributor) => {
           return (
@@ -41,7 +43,7 @@ function RepoContributors({repo}) {
       {loadMore && (
         <div className="load__more__container">
           <SecondaryButton
-            text={'Load More'}
+            text={"Load More"}
             clickEvent={loadMoreContributors}
           />
         </div>
@@ -50,4 +52,4 @@ function RepoContributors({repo}) {
   );
 }
 
-export default RepoContributors
+export default RepoContributors;
