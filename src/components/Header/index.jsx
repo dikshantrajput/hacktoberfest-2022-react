4ba2a03e1e9d7cb8b1b39201c96a592da8ae984d
chiraghm4 <chiragmaski4@gmail.com>
2022-10-21 21:19:17 +0530

    logo added to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,57 +3,60 @@ import { Link } from "react-router-dom";
 import "./index.css";
 import Mode from "../Mode";
 import { useSelector } from "react-redux";
+import logo from "../../assets/hacktober-logo.jpg";
 function Header() {
   const mode = useSelector((state) => state.mode.value);
 
   return (
-    <header className={mode ? "header__dark" : "header__light"}>
+    <header className={mode ? "header__dark" : "header__light"} style={{justifyContent: "spaceAround"}}>
       <div className={mode ? "brand__container" : "brand__container__light"}>
         <Link to="/" className="fw-semibold">
-          Hactoberfest React
+          <img src={logo} alt="" width={"50px"} />
         </Link>
       </div>
-      <nav className="navbar">
-        <ul className="header__nav__list mb-0">
-          <li
-            className={
-              mode
-                ? "header__nav__list__item"
-                : "header__nav__list__item__light"
-            }
-          >
-            <a href="https://hacktoberfest.com/" target="__dk">
-              Hacktoberfest
-            </a>
-          </li>
-          <li
-            className={
-              mode
-                ? "header__nav__list__item"
-                : "header__nav__list__item__light"
-            }
-          >
-            <a
-              href="https://github.com/dikshantrajput/hacktoberfest-2022-react"
-              target="__dk"
+      <div className="text-align-center">
+        <nav className="navbar">
+          <ul className="header__nav__list mb-0">
+            <li
+              className={
+                mode
+                  ? "header__nav__list__item"
+                  : "header__nav__list__item__light"
+              }
             >
-              Repository
-            </a>
-          </li>
-          <li
-            className={
-              mode
-                ? "header__nav__list__item"
-                : "header__nav__list__item__light"
-            }
-          >
-            <Link to="/contributors">Contributors</Link>
-          </li>
-        </ul>
-      </nav>
+              <a href="https://hacktoberfest.com/" target="__dk">
+                Hacktoberfest
+              </a>
+            </li>
+            <li
+              className={
+                mode
+                  ? "header__nav__list__item"
+                  : "header__nav__list__item__light"
+              }
+            >
+              <a
+                href="https://github.com/dikshantrajput/hacktoberfest-2022-react"
+                target="__dk"
+              >
+                Repository
+              </a>
+            </li>
+            <li
+              className={
+                mode
+                  ? "header__nav__list__item"
+                  : "header__nav__list__item__light"
+              }
+            >
+              <Link to="/contributors">Contributors</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <div className="d-flex flex-row">
         <Mode />
-        {mode && <span class="fa fa-moon-o ms-3">Dark Mode</span>}
+        {mode && <span className="fa fa-moon-o ms-3">Dark Mode</span>}
         {mode || <span className="ms-3">Light Mode</span>}
       </div>
     </header>
