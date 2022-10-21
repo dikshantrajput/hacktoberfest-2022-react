8b2aaf4c18d405cdb78b6d0d129530ed5410ff6e
mouryasujit <mouryasujit04@gmail.com>
2022-10-22 00:07:02 +0530

    Adding Animation to new buttons and Updating page design and adding box shadow(inset) to repo cards

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import  store  from "./helpers/store";
+import store from "./helpers/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-
-      <App />
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
